3b879d839a22ff08901cba7012bee238d620d743
its144prelationship <pastalonely@gmail.com>
2024-11-04 11:18:44 +0700

    feat: tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      cream: "#F7EEE6",
+      pink: "#E89D9F",
+      darkpink: "#EA8D8F",
+      darkblue: "#221F4C",
+      red: "#B5363A",
+      green: "#858B1D",
+    },
+    fontFamily: {
+      lalezar: ["Lalezar", "sans-serif"],
+      laomn: ["Laomn", "serif"],
+    },
     extend: {},
   },
   plugins: [],
