511a81cbe0a6bd4a4307c359186a4a9836d27175
its144prelationship <pastalonely@gmail.com>
2024-11-09 00:15:10 +0700

    feat: app

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import HayChainIcon from "../assets/haychain-icon.svg";
+import NavBar from "../assets/navbar.svg";
+
+export function Navbar({ connected, walletAddress, connectWallet }) {
+  return (
+    <div className="relative w-full -top-5">
+      <img src={NavBar} alt="Navbar" className="w-full" />
+      <div className="absolute w-full -top-[20%] flex justify-between">
+        <img src={HayChainIcon} alt="HayChain Icon" className="w-1/6" />
+        <button
+          className="mr-14 my-20 px-3 text-xl text-red font-semibold font-lalezar bg-cream bg-opacity-60 rounded-md border-red border-2 hover:shadow-lg hover:scale-[1.02] duration-200"
+          onClick={connectWallet}
+        >
+          {connected
+            ? `Connected : ${walletAddress.substring(0, 7)}...${walletAddress.slice(-5)}`
+            : "Connect Wallet"}
+        </button>
+      </div>
+    </div>
+  );
+}
